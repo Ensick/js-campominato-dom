7ff750a2973a2da86fa724a3bc28fee92b93a55b
Ensick <fede.censi96@gmail.com>
2022-11-02 13:28:06 +0100

    Numeri Random generati

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
+/* 
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
 
-/* Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-Bonus
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+BONUS:
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
  */
 
 
@@ -18,6 +22,7 @@ let sceltaDifficolta;
 
 
 function AvviaGioco(){
+
 
     if(modalita.value == 1){
 
@@ -33,6 +38,22 @@ function AvviaGioco(){
 
     }
 
+    let arrayNumeriRandom = []
+
+    console.log(arrayNumeriRandom)
+
+    while( arrayNumeriRandom.length < 16 ){
+
+        let numeriRandom = Math.floor(Math.random() * sceltaDifficolta) + 1
+
+        if(!arrayNumeriRandom.includes(numeriRandom)){
+
+            arrayNumeriRandom.push(numeriRandom)
+
+        }
+    }
+
+
     let box = document.getElementById("box")
 
     box.innerHTML = ""
@@ -42,6 +63,7 @@ function AvviaGioco(){
 
         let divNuovo = document.createElement("div")
         divNuovo.classList.add("box-js")
+        divNuovo.innerText = `${x}`;
 
         
         if(sceltaDifficolta == 81){
@@ -54,37 +76,29 @@ function AvviaGioco(){
     
         }
     
+        box.append(divNuovo)
+
+
         divNuovo.addEventListener('click' , function(){
             
-            this.classList.toggle('ms-color-custom')
+            this.classList.add('ms-color-custom')
             console.log(this)
-            console.log(`il numero selezionato è: ${x}`);
-            
-        })
-    
-    
-        if (x % 3 == 0 && x % 5 == 0) {
-    
-            divNuovo.innerHTML = `<h4>${x}</h4>`;
-        
-    
-        } else if (x % 5 == 0) {
-    
-            divNuovo.innerHTML = `<h4>${x}</h4>`;
-    
-            
-        } else if (x % 3 == 0) {
-    
-            divNuovo.innerHTML = `<h4>${x}</h4>`;
-            
-    
-        } else {
-    
-            divNuovo.innerHTML = `<h4>${x}</h4>`;
+            console.log(x);
+
            
-        }
-    
-        box.append(divNuovo)
+            if(arrayNumeriRandom.includes(x)){
+
+                for(k = 0; k < sceltaDifficolta; k++){
+
+                    divNuovo.classList.add('ms-color-red')
+                    console.log(this.innerHTML)
+                }
+                
+            }
+
+         
+  
+        })
     
     }
 
